54564202874c1f23d6a23585917708d68e2ed6d9
DmitryK <dmitkantemir@gmail.com>
2021-07-13 22:31:55 +0300

    правка 1

--- index.php
+++ index.php
@@ -1,38 +1,34 @@
 <?php
  $name= null;
- $password = null;
+ $surname = null;
  $age = null;
 
  if (!empty($_POST)) {
-     $name = isset($_POST['login']) ? $_POST['login'] : null;
-     $password = isset($_POST['password']) ? $_POST['password'] : null;
+     $name = isset($_POST['Name']) ? $_POST['Name'] : null;
+     $surname= isset($_POST['Surname']) ? $_POST['Surname'] : null;
      $age = isset($_POST['age']) ? $_POST['age'] : null;
  }
   
  if (!empty($_GET)) {
-     $name = isset($_GET['login']) ? $_GET['login'] : null;
-     $password = isset($_GET['password']) ? $_GET['password'] : null;
+     $name = isset($_GET['Name']) ? $_GET['Name'] : null;
+     $surname = isset($_GET['Surname']) ? $_GET['Surname'] : null;
      $age = isset($_GET['age']) ? $_GET['age'] : null;
  }
    
 ?>
 
-<html>
-<head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-
 
 <form method="post">
     <div class="mb-3">
-        <label for="exampleInputlogin" class="form-label">Login</label>
-        <input required type="text" name="login" class="form-control">
-        <div class="form-text">Введите логин.</div>
+        <label for="exampleInputname" class="form-label"> Name</label>
+        <input required type="text" name="Name" class="form-control">
+        <div class="form-text">Введите ваше имя.</div>
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-        <div class="form-text">Введите пароль.</div>
+        <label for="exampleInputSurname" class="form-label">Surname</label>
+        <input type="Surname" name="Surname" class="form-control" id="exampleInputSurname">
+        <div class="form-text">Введите фамилию.</div>
     </div class="mb-3">
         <label for="exampleInputAge" class="form-label">Age</label>
         <input required type="text" name="age" class="form-control" id="exampleInputAge1">
@@ -41,20 +37,12 @@
 
 
     <button type="submit" class="btn btn-primary">Отправить</button>
-    </style> 
-
-    <body>
-
+    
  
 <h1><?= $_GET['test']; ?></h1>
 
 <div>
     <p> <strong>Name: </strong><?= $name ?></p>
-    <p><strong>Password: </strong><?= $password ?></p>
+    <p><strong>Surname: </strong><?= $surname ?></p>
     <p><strong>Age: </strong><?= $age ?></p>
 </div>
-    
-    </form>
- 
-</body>
-</html>
